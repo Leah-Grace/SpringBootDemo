338b1e5ef7502a9be691666aa9e12cdebad19c47
Leah Grace <Leahgdg@hotmail.com>
2022-03-28 12:55:54 -0400

    "Demo controller crated/tested"

---
+++ src/main/java/com/LeahGrace/demo/DemoServer/DemoMessage.java
@@ -0,0 +1,9 @@
+package com.LeahGrace.demo.DemoServer;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public class DemoMessage {
+
+}
